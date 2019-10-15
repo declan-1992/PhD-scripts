01424afedf9811ed3e9b04462f65aa7a406e1b9e
declan-1992 <55177948+declan-1992@users.noreply.github.com>
2019-10-15 10:31:37 +0100

    Update Create_defects.sh

--- Create_defects.sh
+++ Create_defects.sh
@@ -8,6 +8,6 @@ i=1    ## set the iteration to 1
 
 while [[ $i -le $d ]]    ## create while loop that keeps removing lines (atomic postions) from xyz file until the total number of defects (d) is removed
 do
-sed -i $((2 + RANDOM % 36000))d start.xyz    ## sed command to remove lines (atomic positions) from xyz file
+sed -i $((2 + RANDOM % 36000))d start.xyz    ## sed command to remove lines (atomic positions) from xyz file. Start from lowest index line to remove is 3 ( First two lines are text that need to remain). File has over 36000 lines
 i=$[$i + 1]    ## increase the iteration variable by 1
 done 
